e2a872a307c07403df31a0cccb974b34a73b7eae
Vlad Morozov <rapic007@yandex.ru>
2024-02-01 19:29:05 +0300

    Initial Commit

---
+++ PhotosGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosGallery
+//
+//  Created by Влад  on 1.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
